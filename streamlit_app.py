5e42b0ca173b99b23d32fc6996e282b1113635da
hongbin-xu <36011407+hongbin-xu@users.noreply.github.com>
2023-10-10 14:19:34 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -107,7 +107,7 @@ def lonExtrac(data, data_filtered, id):
 def surfOrigin(data):
     # hover information
     z = data["height"].values.reshape([425, -1])
-    fig = go.Figure(data=[go.Surface(z=z, x=np.arange(4096), y=np.arange(425))])
+    fig = go.Figure(data=[go.Surface(z=z, x=np.arange(4096), y=np.arange(425), hoverlabel = dict(x="transID", y = "lonID", z = "height"))])
     fig.update_layout(title = "Original", scene=dict(xaxis_title="Transverse ID", yaxis_title="Longitudinal ID", zaxis_title="height"),template="plotly", height = 600)
 
     #fig['layout']['xaxis']['autorange'] = "reversed"
